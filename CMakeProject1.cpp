e196345686d38339582c6bfb44be0b7a53045687
olimjony <tabarzodaolimjon@gmail.com>
2025-11-07 12:03:31 +0500

    first commit

---
+++ CMakeProject1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
+#include <chrono>
+
+int sumVector(const std::vector<int>& a, int start, int end) {
+    int S = 0;
+    for (int i = start; i < end; i++) {
+        S += a[i];
+    }
+    return S;
+}
+
+int main() {
+    const int size = 1000000;
+    std::vector<int> vec(size);
+    std::srand(std::time(0));
+    for (int i = 0; i < size; i++) {
+        vec[i] = std::rand() % 100;
+    }
+
+    auto st_single = std::chrono::high_resolution_clock::now();
+    int sum_single = sumVector(vec, 0, size);
+    auto end_single = std::chrono::high_resolution_clock::now();
+    double time_single = std::chrono::duration<double>(end_single - st_single).count();
+    std::cout << "one threaded sum: " << sum_single << std::endl;
+    std::cout << "one threaded time: " << time_single << " seconds" << std::endl;
+
+    int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+    auto st_multi = std::chrono::high_resolution_clock::now();
+    std::thread t1([&sum1, &vec]() { sum1 = sumVector(vec, 0, 250000); });
+    std::thread t2([&sum2, &vec]() { sum2 = sumVector(vec, 250000, 500000); });
+    std::thread t3([&sum3, &vec]() { sum3 = sumVector(vec, 500000, 750000); });
+    std::thread t4([&sum4, &vec]() { sum4 = sumVector(vec, 750000, 1000000); });
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    int sum_multi = sum1 + sum2 + sum3 + sum4;
+    auto end_multi = std::chrono::high_resolution_clock::now();
+    double time_multi = std::chrono::duration<double>(end_multi - st_multi).count();
+    std::cout << "many threaded sum: " << sum_multi << std::endl;
+    std::cout << "many threaded time: " << time_multi << " seconds" << std::endl;
+
+    return 0;
+}
